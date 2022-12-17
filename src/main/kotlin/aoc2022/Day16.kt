7b9dd37ee844b871f9a697ba325c3bca6153276f
Tommy Kang <tommykang77@naver.com>
2022-12-17 23:58:43 +0000

    Improve Day16 Part 2

--- src/main/kotlin/aoc2022/Day16.kt
+++ src/main/kotlin/aoc2022/Day16.kt
@@ -18,19 +18,13 @@ class Day16(filename: String) {
         if (time <= 0.toLong()) {
             return 0
         }
-        if (memo.contains(Triple(valve, time, opened))) {
-            return memo[Triple(valve, time, opened)]!!
+        if (memo.contains(Triple(valve, time, opened.toSet()))) {
+            return memo[Triple(valve, time, opened.toSet())]!!
         }
 
         var score: Long = 0
         if (!opened.contains(valve) && flowRate.getOrDefault(valve, Long.MIN_VALUE) != 0.toLong()) {
             opened.add(valve)
-//            for (nextValve in leadsTo.getOrDefault(valve, listOf())) {
-//                score = score.coerceAtLeast(
-//                    (time - 1) * flowRate.getOrDefault(valve, Long.MIN_VALUE) +
-//                            maxScore(nextValve, time - 2, opened)
-//                )
-//            }
             score = score.coerceAtLeast(
                 (time - 1) * flowRate.getOrDefault(valve, Long.MIN_VALUE) +
                         maxScore(valve, time - 1, opened)
@@ -42,119 +36,49 @@ class Day16(filename: String) {
                 maxScore(nextValve, time - 1, opened)
             )
         }
-        memo[Triple(valve, time, opened)] = score
+        memo[Triple(valve, time, opened.toSet())] = score
         return score
     }
 
     fun part1(): Long =
         maxScore("AA", 30, mutableSetOf())
 
-    // doesnt work
-    val memo2 = mutableMapOf<Triple<Pair<String, String>, Long, Set<String>>, Long>()
-    fun maxScore2(valve: String, valve2: String, time: Long, opened: MutableSet<String>): Long {
+    // works for example
+    // timeout on real input
+    val cmemo = mutableMapOf<Triple<String, Long, Set<String>>, Long>()
+
+    fun maxScore3(valve: String, time: Long, opened: MutableSet<String>): Long {
         if (time <= 0.toLong()) {
-            return 0
+            return maxScore("AA", 26, opened)
         }
-        if (memo2.contains(Triple(valve to valve2, time, opened))) {
-            return memo2[Triple(valve to valve2, time, opened)]!!
+        if (cmemo.contains(Triple(valve, time, opened.toSet()))) {
+            return cmemo[Triple(valve, time, opened.toSet())]!!
         }
 
         var score: Long = 0
-        if (!opened.contains(valve) && flowRate.getOrDefault(valve, Long.MIN_VALUE) != 0.toLong() && !opened.contains(valve2) && flowRate.getOrDefault(valve2, Long.MIN_VALUE) != 0.toLong() && valve != valve2) {
-            opened.add(valve)
-            opened.add(valve2)
-            score = score.coerceAtLeast(
-                (time - 1) * flowRate.getOrDefault(valve, Long.MIN_VALUE) +
-                        (time - 1) * flowRate.getOrDefault(valve2, Long.MIN_VALUE) +
-                        maxScore2(valve, valve2, time - 1, opened)
-            )
-            opened.remove(valve)
-            opened.remove(valve2)
-        }
         if (!opened.contains(valve) && flowRate.getOrDefault(valve, Long.MIN_VALUE) != 0.toLong()) {
             opened.add(valve)
-            for (nextValve2 in leadsTo.getOrDefault(valve2, listOf())) {
-                score = score.coerceAtLeast(
-                    (time - 1) * flowRate.getOrDefault(valve, Long.MIN_VALUE) +
-                            maxScore2(valve, nextValve2, time - 1, opened)
-                )
-
-            }
+            score = score.coerceAtLeast(
+                (time - 1) * flowRate.getOrDefault(valve, Long.MIN_VALUE) +
+                        maxScore3(valve, time - 1, opened)
+            )
             opened.remove(valve)
         }
-        if (!opened.contains(valve2) && flowRate.getOrDefault(valve2, Long.MIN_VALUE) != 0.toLong()) {
-            opened.add(valve2)
-            for (nextValve in leadsTo.getOrDefault(valve, listOf())) {
-                score = score.coerceAtLeast(
-                            (time - 1) * flowRate.getOrDefault(valve2, Long.MIN_VALUE) +
-                            maxScore2(nextValve, valve2, time - 1, opened)
-                )
-            }
-            opened.remove(valve2)
-        }
         for (nextValve in leadsTo.getOrDefault(valve, listOf())) {
-            for (nextValve2 in leadsTo.getOrDefault(valve2, listOf())) {
-                score = score.coerceAtLeast(
-                    maxScore2(nextValve, nextValve2, time - 1, opened)
-                )
-            }
+            score = score.coerceAtLeast(
+                maxScore3(nextValve, time - 1, opened)
+            )
         }
-        memo2[Triple(valve to valve2, time, opened)] = score
+        cmemo[Triple(valve, time, opened.toSet())] = score
         return score
     }
 
     fun part2(): Long =
-        maxScore2("AA", "AA", 26, mutableSetOf())
+        maxScore3("AA", 26, mutableSetOf())
 }
 
 fun main() {
     val sol = Day16("src/main/resources/2022/Day16Input.txt")
     println(sol.part1())
+    println(sol.part2())
 }
-
-// failed greedy approach
-//    fun getValveScores(startValve: String, startTime: Long): Map<String, Pair<Long, List<String>>> {
-//        val valveScores = mutableMapOf<String, Pair<Long, List<String>>>()
-//        val visited = mutableSetOf<String>(startValve)
-//        val queue = ArrayDeque<Triple<String, Long, List<String>>>()
-//        queue.addLast(Triple(startValve, startTime, listOf<String>()))
-//        while (queue.isNotEmpty()) {
-//            val (valve, time, path) = queue.removeFirst()
-//            val newPath = path.plus(valve)
-//            val score = if (!opened.contains(valve)) (time- 1) * flowRate.getOrDefault(valve, Long.MIN_VALUE) else 0
-//            valveScores[valve] = score to newPath
-//
-//            if (time > 1) {
-//                for (nextValve in leadsTo.getOrDefault(valve, listOf())) {
-//                    if (!visited.contains(nextValve)) {
-//                        queue.addLast(Triple(nextValve, time - 1, newPath))
-//                        visited.add(nextValve)
-//                    }
-//                }
-//            }
-//        }
-//        return valveScores
-//    }
-//
-//    fun simulate(startValve: String, startTime: Long): Long {
-//        var total: Long = 0
-//        var accumulated: Long = 0
-//        var time = startTime
-//        var valve = startValve
-//        while (time > 0) {
-//            total += accumulated
-//            val scores = getValveScores(valve, time)
-//            val best = scores.maxBy { it.value.first }
-//            time -= 1
-//            if (best.key == valve) {
-//                accumulated += flowRate.getOrDefault(valve, Long.MIN_VALUE)
-//                opened.add(valve)
-//            }
-//            else {
-//                if (best.value.second.size > 1) {
-//                    valve = best.value.second[1]
-//                }
-//            }
-//        }
-//        return total
-//    }
