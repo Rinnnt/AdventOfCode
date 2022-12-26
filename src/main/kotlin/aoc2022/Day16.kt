7c6bc8ececccb3b0acf2caf586d572e567e6ab9a
Tommy Kang <tommykang77@naver.com>
2022-12-26 02:21:38 +0000

    Complete Day 16 Part 2

--- src/main/kotlin/aoc2022/Day16.kt
+++ src/main/kotlin/aoc2022/Day16.kt
@@ -12,29 +12,55 @@ class Day16(filename: String) {
         val words = it.split(" ")
         it.split(" ")[1] to it.substringAfter("valves ").substringAfter("valve ").split(", ")
     }
-    val memo = mutableMapOf<Triple<String, Long, Set<String>>, Long>()
 
-    fun maxScore(valve: String, time: Long, opened: MutableSet<String>): Long {
-        if (time <= 0.toLong()) {
-            return 0
+    val shortestDistance = getDistances()
+    val leadsToUseful =
+        leadsTo.filter { it.key == "AA" || flowRate[it.key] != 0.toLong() }
+            .mapValues { (k, _) -> leadsTo.keys.filter { flowRate[it] != 0.toLong() && it != k } }
+
+    fun getDistances(): Map<Pair<String, String>, Int> {
+        val distances = mutableMapOf<Pair<String, String>, Int>()
+        leadsTo.keys.filter { it == "AA" || flowRate[it] != 0.toLong() }.forEach { valve ->
+            val queue = ArrayDeque<Pair<String, Int>>()
+            queue.add(Pair(valve, 0))
+            val visited = mutableSetOf(valve)
+
+            while (queue.isNotEmpty()) {
+                val (v, t) = queue.removeFirst()
+
+                if (valve != v && flowRate[v] != 0.toLong()) {
+                    distances[Pair(valve, v)] = t
+                }
+
+                for (nv in leadsTo[v]!!) {
+                    if (!visited.contains(nv)) {
+                        queue.addLast(Pair(nv, t + 1))
+                        visited.add(nv)
+                    }
+                }
+            }
         }
+        return distances
+    }
+
+    val memo = mutableMapOf<Triple<String, Long, Set<String>>, Long>()
+    fun maxScore(valve: String, time: Long, opened: MutableSet<String>): Long {
         if (memo.contains(Triple(valve, time, opened.toSet()))) {
             return memo[Triple(valve, time, opened.toSet())]!!
         }
 
         var score: Long = 0
-        if (!opened.contains(valve) && flowRate.getOrDefault(valve, Long.MIN_VALUE) != 0.toLong()) {
-            opened.add(valve)
-            score = score.coerceAtLeast(
-                (time - 1) * flowRate.getOrDefault(valve, Long.MIN_VALUE) +
-                        maxScore(valve, time - 1, opened)
-            )
-            opened.remove(valve)
-        }
-        for (nextValve in leadsTo.getOrDefault(valve, listOf())) {
-            score = score.coerceAtLeast(
-                maxScore(nextValve, time - 1, opened)
-            )
+
+        for (nextValve in leadsToUseful[valve]!!.filter { !opened.contains(it) }) {
+            val requiredTime = shortestDistance[Pair(valve, nextValve)]!!
+            if (requiredTime + 1 < time) {
+                opened.add(nextValve)
+                score = score.coerceAtLeast(
+                    (time - requiredTime - 1) * flowRate[nextValve]!! +
+                            maxScore(nextValve, time - requiredTime - 1, opened)
+                )
+                opened.remove(nextValve)
+            }
         }
         memo[Triple(valve, time, opened.toSet())] = score
         return score
@@ -43,38 +69,32 @@ class Day16(filename: String) {
     fun part1(): Long =
         maxScore("AA", 30, mutableSetOf())
 
-    // works for example
-    // timeout on real input
     val cmemo = mutableMapOf<Triple<String, Long, Set<String>>, Long>()
-
-    fun maxScore3(valve: String, time: Long, opened: MutableSet<String>): Long {
-        if (time <= 0.toLong()) {
-            return maxScore("AA", 26, opened)
-        }
+    fun maxScore2(valve: String, time: Long, opened: MutableSet<String>): Long {
         if (cmemo.contains(Triple(valve, time, opened.toSet()))) {
             return cmemo[Triple(valve, time, opened.toSet())]!!
         }
 
-        var score: Long = 0
-        if (!opened.contains(valve) && flowRate.getOrDefault(valve, Long.MIN_VALUE) != 0.toLong()) {
-            opened.add(valve)
-            score = score.coerceAtLeast(
-                (time - 1) * flowRate.getOrDefault(valve, Long.MIN_VALUE) +
-                        maxScore3(valve, time - 1, opened)
-            )
-            opened.remove(valve)
+        var score: Long = maxScore("AA", 26, opened)
+//        val nextScore = maxScore("AA", 26, opened)
+        for (nextValve in leadsToUseful[valve]!!.filter { !opened.contains(it) }) {
+            val requiredTime = shortestDistance[Pair(valve, nextValve)]!!
+            if (requiredTime + 1 < time) {
+                opened.add(nextValve)
+                score = score.coerceAtLeast(
+                    (time - requiredTime - 1) * flowRate[nextValve]!! +
+                            maxScore2(nextValve, time - requiredTime - 1, opened)
+                )
+                opened.remove(nextValve)
+            }
         }
-        for (nextValve in leadsTo.getOrDefault(valve, listOf())) {
-            score = score.coerceAtLeast(
-                maxScore3(nextValve, time - 1, opened)
-            )
-        }
+//        score = score.coerceAtLeast(nextScore)
         cmemo[Triple(valve, time, opened.toSet())] = score
         return score
     }
 
     fun part2(): Long =
-        maxScore3("AA", 26, mutableSetOf())
+        maxScore2("AA", 26, mutableSetOf())
 }
 
 fun main() {
