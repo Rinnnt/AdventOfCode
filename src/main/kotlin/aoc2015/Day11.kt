e0577a9ae301d07797316dbdc92d05a245a29d7c
Tommy Kang <tommykang77@naver.com>
2023-01-08 13:49:54 +0000

    Complete Day11 2015

--- src/main/kotlin/aoc2015/Day11.kt
+++ src/main/kotlin/aoc2015/Day11.kt
@@ -3,37 +3,70 @@ package aoc2015
 import java.io.File
 
 class Day11(filename: String) {
-    val oldPassword = File(filename).bufferedReader().readLine().toCharArray()
+    private val oldPassword = File(filename).bufferedReader().readLine().toCharArray()
 
-    fun increment(a: CharArray) {
-        var i = a.size - 1
-        a[i] = ((a[i].code - 96) % 26 + 97).toChar()
+    private fun increment(a: CharArray, from: Int) {
+        var i = a.size - from
+        var newChar = ((a[i].code - 96) % 26 + 97).toChar()
+        if (newChar == 'i' || newChar == 'o' || newChar == 'l') {
+            newChar = ((newChar.code + 1).toChar())
+        }
+        a[i] = newChar
         while (i > 0 && a[i] == 'a') {
             i -= 1
-            a[i] = ((a[i].code - 96) % 26 + 97).toChar()
+            var newChar = ((a[i].code - 96) % 26 + 97).toChar()
+            if (newChar == 'i' || newChar == 'o' || newChar == 'l') {
+                newChar = ((newChar.code + 1).toChar())
+            }
+            a[i] = newChar
         }
     }
 
-    fun nextValid(a: CharArray) {
-        while (a.contains('i') || a.contains('o') || a.contains('l')) {
-            increment(a)
+
+    private fun nextValidPassword(a: CharArray) {
+        if (a.contains('i') || a.contains('o') || a.contains('l')) {
+            val idx = a.indexOfFirst { "iol".contains(it) }
+            a[idx] = (a[idx].code + 1).toChar()
+            for (j in idx until a.size) {
+                a[j] = 'a'
+            }
         }
-    }
+        val i = a.size - 5
+        val expected = listOf(a[i], a[i], (a[i].code + 1).toChar(), (a[i].code + 2).toChar(), (a[i].code + 2).toChar())
+        if (a[i] == 'y' || a[i] == 'z') {
+            a[i] = 'a'
+            a[i + 1] = 'a'
+            a[i + 2] = 'b'
+            a[i + 3] = 'c'
+            a[i + 4] = 'c'
 
-    fun validPassword(a: CharArray): Boolean {
-        if (a.contains('i') || a.contains('o') || a.contains('l')) return false
-        if (!a.toList().windowed(3).any { it[2].code == it[1].code + 1 && it[1].code == it[0].code + 1 }) return false
-
-        val regex = """(\w)\1.*(\w)\2""".toRegex()
-        if (!regex.containsMatchIn(a.toString())) return false
-        return true
+            increment(a, 6)
+        } else if (a.joinToString("").substring(i, a.size) > expected.joinToString("")) {
+            a[i] = (a[i].code + 1).toChar()
+            nextValidPassword(a)
+        } else if ("ghijklmno".contains(a[i])) {
+            a[i] = 'p'
+            a[i + 1] = 'p'
+            a[i + 2] = 'q'
+            a[i + 3] = 'r'
+            a[i + 4] = 'r'
+        } else {
+            a[i + 1] = a[i]
+            a[i + 2] = (a[i + 1].code + 1).toChar()
+            a[i + 3] = (a[i + 2].code + 1).toChar()
+            a[i + 4] = a[i + 3]
+        }
     }
 
     fun part1(): String {
-        while (!validPassword(oldPassword)) {
-            increment(oldPassword)
-        }
-        return oldPassword.toString()
+        nextValidPassword(oldPassword)
+        return oldPassword.joinToString("")
+    }
+
+    fun part2(): String {
+        increment(oldPassword, 1)
+        nextValidPassword(oldPassword)
+        return oldPassword.joinToString("")
     }
 
 }
@@ -41,4 +74,5 @@ class Day11(filename: String) {
 fun main() {
     val sol = Day11("src/main/resources/2015/Day11Input.txt")
     println(sol.part1())
+    println(sol.part2())
 }
